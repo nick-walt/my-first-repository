6f3ff27f55cacccdee456d82bc6441e88f91ad3f
nickwalt <nick-walt@users.noreply.github.com>
2016-08-18 15:43:59 +1000

    First scraps

---
+++ a.js
@@ -0,0 +1,13 @@
+function sleepCheck (numHours) {
+    var plenty = "You're getting plenty of sleep!";
+    var much = "Maybe even too much!";
+    var more = "Get some more shut eye!";
+    if (numHours >= 8 && numHours <= 9) {
+        return plenty;
+    } else if (numHours > 9){
+        return plenty + " " + much;
+    } else {
+        return more;
+    }
+}
+sleepCheck (10);
